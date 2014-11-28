a777528f807490e1e45908490e21b0c9ec0e482c
JongEun Lee <yomybaby@yomybaby.com>
2014-11-28 20:12:53 +0900

    get, post 확인

--- backbone-localStorage-todos/app/scripts/models/todo-model.js
+++ backbone-localStorage-todos/app/scripts/models/todo-model.js
@@ -10,6 +10,13 @@
 	        title: '',
 	        completed: false
 	    },
+	    idAttribute : 'objectId',
+	    sync : function(method, model, options){
+	    	options.beforeSend = function(xhr){
+	    		xhr.setRequestHeader('TDCProjectKey','aed4cc0d-9cbd-45ee-8da1-ab958e716622');
+	    	};
+	    	return Backbone.sync(method, model, options);
+	    },
 
 	    toggle: function () {
 	        this.save({
