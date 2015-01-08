142097f49822363689dcf1ed2cf0a17d629f96a4
Jong Eun Lee <yomybaby@yomybaby.com>
2015-01-08 15:34:51 +0900

    delete angular examples

--- angular-localStorage-urlShortener/app/scripts/services/UrlResource.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('angularLocalStorageUrlShortenerApp')
-  .factory('UrlResource', function UrlResource($resource) {
-    return $resource('https://www.googleapis.com/urlshortener/v1/url');
-  });
